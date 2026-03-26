b2c9195fb29648b2d22840f81de3bc687449f4b0
R-Grigala <r.grigalashvili777@gmail.com>
2026-03-26 17:33:44 +0400

    config meil sender

--- src/services/email_sender.py
+++ src/services/email_sender.py
@@ -5,21 +5,41 @@ BASE_PATH_DEFAULT = "/home/sysop/shakemap_profiles/default/data"
 MAIL_LIST_PATH = os.path.join(os.path.dirname(__file__), "mail_list")
 
 def email_sender(event_id, parsed_data, base_path=BASE_PATH_DEFAULT):
-    
+
     email_title = f"მიწისძვრა - {event_id}"
-    email_message = f"""
-        ShakeMap მზად არის event: {event_id} შესამოწმებლად
+    email_message = f'''
+    ShakeMap მზად არის event: {event_id} -თვის.
 
-        Event ID: {event_id}
-        Time: {parsed_data['time']}
-        Location: {parsed_data['latitude']}, {parsed_data['longitude']}
-        Depth: {parsed_data['depth']} km
-        Magnitude: {parsed_data['ml']}
-    """
-    product_path = os.path.join(base_path, event_id, "current/products")
-    attachments = [os.path.join(product_path, f) for f in ["pga.jpg", "pgv.jpg", "intensity.jpg"] if os.path.exists(os.path.join(product_path, f))]
-    if attachments:
-        ies_mail_sender.send_mail(MAIL_LIST_PATH, email_title, email_message, attachments)
-        logging.info(f"Email sent for {event_id}")
+    დეტალური ინფორმაცია მიწისძვრის შესახებ:\n\n
+    Event ID: {event_id}
+    Time: {parsed_data["time"]}
+    Location: {parsed_data["latitude"]}, {parsed_data["longitude"]}
+    Depth: {parsed_data["depth"]} km
+    Magnitude: {parsed_data["ml"]}
+'''
 
+    base_path = f"/home/sysop/shakemap_profiles/default/data/{event_id}/current/products"
+
+    # ფაილები
+    attachments = [
+        os.path.join(base_path, "pga.jpg"),
+        os.path.join(base_path, "pgv.jpg"),
+        os.path.join(base_path, "intensity.jpg"),
+    ]
+
+    # მხოლოდ არსებული ფაილები
+    existing_files = [f for f in attachments if os.path.exists(f)]
+
+    if not existing_files:
+        logging.error("არცერთი attachment ვერ მოიძებნა")
+        return
+
+    ies_mail_sender.send_mail(
+        MAIL_LIST_PATH,
+        email_title,
+        email_message,
+        attachments=existing_files
+    )
+
+    logging.info(f"მეილი გაიგზავნა: {event_id}")
 
